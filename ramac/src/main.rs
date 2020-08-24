d5eaa7484a9a52ea559ef019d789521435da9dfa
PJTSearch <pjtsignups@gmail.com>
2020-08-24 08:23:40 -0700

    feat: add commit_transaction

--- ramac/src/main.rs
+++ ramac/src/main.rs
@@ -1,24 +1,7 @@
-use alpm::Db;
-use alpm::Pkg;
-use std::sync::Mutex;
-use std::sync::Arc;
-use std::cell::RefCell;
 use std::rc::Rc;
-use alpm::{Alpm,TransFlag,SigLevel,Package};
+use alpm::{Alpm,TransFlag,SigLevel};
 
 fn main() {
-    // let alpm = Alpm::new("/","/var/lib/pacman");
-    // match alpm{
-    //     Ok(mut alpm)=>{
-    //         {get_db(&mut alpm);}
-    //         Pk::new(&mut alpm,"vi".to_owned()).install();
-    //         // alpm.set_use_syslog(true);
-
-    //         // let pkg = get_pkg(&mut alpm,"vi".to_owned());
-    //         // install(&mut alpm, pkg)
-    //     },
-    //     Err(error)=>println!("{:#?}",error)            
-    // }
     match Racman::new() {
         Ok(mut racman)=>{
             racman.register_syncdb("core", "http://mirrors.evowise.com/archlinux/core/os/x86_64/");
@@ -27,46 +10,10 @@ fn main() {
         },
         Err(error)=>panic!(error)
     }
-
-    // println!("Hello, world!");
-}
-
-fn get_db(alpm:&mut Alpm){
-    let db = alpm.register_syncdb_mut("core", SigLevel::NONE).unwrap();
-    db.add_server("http://mirrors.evowise.com/archlinux/core/os/x86_64/")
-    .unwrap();
-}
-
-// fn install(alpm:&mut Alpm,pkg:Package){
-//     alpm.trans_init(TransFlag::NONE).expect("couldn't init transaction");
-//     alpm.trans_add_pkg(pkg).expect("couldn't add pkg to transaction");
-//     alpm.trans_prepare().expect("couldn't prepare transaction");
-//     alpm.trans_commit().expect("couldn't run transaction");
-// }
-
-struct Pk<'a> {
-    alpm:&'a mut Alpm,
-    name:String
-}
-
-impl Pk<'_> {
-    fn new<'a>(alpm:&'a mut Alpm,name:String)->Pk{
-        return Pk{
-            alpm,
-            name
-        }
-    }
-    fn install(&mut self){
-        let db = self.alpm.syncdbs().find(|db| db.name() == "core").unwrap();
-        let package = db.pkg(&self.name).unwrap();
-        self.alpm.trans_init(TransFlag::NONE).expect("couldn't init transaction");
-        self.alpm.trans_add_pkg(package).expect("couldn't add pkg to transaction");
-        self.alpm.trans_prepare().expect("couldn't prepare transaction");
-        self.alpm.trans_commit().expect("couldn't run transaction");
-    }
 }
 
 trait Transaction {
+    fn commit(&self,alpm:&mut Alpm);
 }
 
 #[derive(Clone)]
@@ -75,11 +22,20 @@ struct InstallTransaction{
     name:String
 }
 
-impl Transaction for InstallTransaction {}
+impl Transaction for InstallTransaction {
+    fn commit(&self,alpm:&mut Alpm){
+        let db = alpm.syncdbs().find(|db| db.name() == self.repo_name).unwrap();
+        let package = db.pkg(&self.name).unwrap();
+        alpm.trans_init(TransFlag::NONE).expect("couldn't init transaction");
+        alpm.trans_add_pkg(package).expect("couldn't add pkg to transaction");
+        alpm.trans_prepare().expect("couldn't prepare transaction");
+        alpm.trans_commit().expect("couldn't run transaction");
+    }
+}
 
 struct Racman {
     alpm:Alpm,
-    transactions:Vec<Rc<Transaction>>
+    transactions:Vec<Rc<dyn Transaction>>
 }
 
 impl Racman {
@@ -98,14 +54,14 @@ impl Racman {
             .unwrap();
     }
     fn add_install(&mut self,repo_name:&str,name:&str){
-        // let db = self.alpm.syncdbs().find(|db| db.name() == repo_name).unwrap();
-        // let package = db.pkg(name).unwrap();
         self.transactions.push(Rc::new(InstallTransaction{repo_name:repo_name.to_owned().clone(),name:name.to_owned().clone()}));
-        // self.alpm.trans_init(TransFlag::NONE).expect("couldn't init transaction");
-        // self.alpm.trans_add_pkg(package).expect("couldn't add pkg to transaction");
     }
     fn commit_transaction(&mut self){
-        self.alpm.trans_prepare().expect("couldn't prepare transaction");
-        self.alpm.trans_commit().expect("couldn't run transaction");
+        let commit = |transactions:&Vec<Rc<dyn Transaction>>,alpm:&mut Alpm| {
+            transactions.iter().for_each(|transaction|{
+                transaction.commit(alpm);
+            });
+        };
+        commit(&self.transactions,&mut self.alpm);
     }
 }
