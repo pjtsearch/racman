e891b96c7c83450003fb9b69956ec1b13abfb153
PJTSearch <pjtsignups@gmail.com>
2020-08-23 18:59:29 -0700

    chore: start racman

---
+++ ramac/src/main.rs
@@ -0,0 +1,58 @@
+use alpm::Db;
+use alpm::Pkg;
+use std::sync::Mutex;
+use std::sync::Arc;
+use std::cell::RefCell;
+use alpm::{Alpm,TransFlag,SigLevel,Package};
+
+fn main() {
+    let alpm = Alpm::new("/","/var/lib/pacman");
+    match alpm{
+        Ok(mut alpm)=>{
+            {get_db(&mut alpm);}
+            Pk::new(&mut alpm,"vi".to_owned()).install();
+            // alpm.set_use_syslog(true);
+
+            // let pkg = get_pkg(&mut alpm,"vi".to_owned());
+            // install(&mut alpm, pkg)
+        },
+        Err(error)=>println!("{:#?}",error)            
+    }
+
+    // println!("Hello, world!");
+}
+
+fn get_db(alpm:&mut Alpm){
+    let db = alpm.register_syncdb_mut("core", SigLevel::NONE).unwrap();
+    db.add_server("http://mirrors.evowise.com/archlinux/core/os/x86_64/")
+    .unwrap();
+}
+
+// fn install(alpm:&mut Alpm,pkg:Package){
+//     alpm.trans_init(TransFlag::NONE).expect("couldn't init transaction");
+//     alpm.trans_add_pkg(pkg).expect("couldn't add pkg to transaction");
+//     alpm.trans_prepare().expect("couldn't prepare transaction");
+//     alpm.trans_commit().expect("couldn't run transaction");
+// }
+
+struct Pk<'a> {
+    alpm:&'a mut Alpm,
+    name:String
+}
+
+impl Pk<'_> {
+    fn new<'a>(alpm:&'a mut Alpm,name:String)->Pk{
+        return Pk{
+            alpm,
+            name
+        }
+    }
+    fn install(&mut self){
+        let db = self.alpm.syncdbs().find(|db| db.name() == "core").unwrap();
+        let package = db.pkg(&self.name).unwrap();
+        self.alpm.trans_init(TransFlag::NONE).expect("couldn't init transaction");
+        self.alpm.trans_add_pkg(package).expect("couldn't add pkg to transaction");
+        self.alpm.trans_prepare().expect("couldn't prepare transaction");
+        self.alpm.trans_commit().expect("couldn't run transaction");
+    }
+}
