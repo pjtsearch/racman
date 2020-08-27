e9ebda9783e2586b20383f805c374521078b2abb
PJTSearch <pjtsignups@gmail.com>
2020-08-26 18:21:23 -0700

    feat(InstallTransaction): replace panic with result

--- racman/src/transaction/install.rs
+++ racman/src/transaction/install.rs
@@ -1,7 +1,7 @@
 use alpm::Error;
 use alpm::Alpm;
 use crate::transaction::Transaction;
-use anyhow::Result;
+use anyhow::{Result,Context};
 
 #[derive(Clone)]
 pub struct InstallTransaction{
@@ -12,7 +12,7 @@ impl Transaction for InstallTransaction {
     fn add(&self,alpm:&mut Alpm)->Result<()>{
         let db = alpm.syncdbs().find(|db| db.pkg(&self.name).is_ok()).ok_or_else(||Error::PkgRepoNotFound)?;
         let package = db.pkg(&self.name)?;
-        alpm.trans_add_pkg(package).expect("couldn't add pkg to transaction");
+        alpm.trans_add_pkg(package).context("couldn't add pkg to transaction")?;
         Ok(())
     }
 }
